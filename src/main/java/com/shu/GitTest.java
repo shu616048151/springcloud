136bb9f39597c44676729f522fe1858cb1db7c50
shu616048151 <616048151@qq.com>
2019-04-11 10:32:23 +0800

    主分支第一次分支提交代码

--- src/main/java/com/shu/GitTest.java
+++ src/main/java/com/shu/GitTest.java
@@ -3,6 +3,6 @@ package com.shu;
 public class GitTest{
     public static void main(String[] args){
         System.out.println("Git 修改文件测试提交");
-     
+        System.out.println("这是主分支开始第一次提交代码");
     }
 }
