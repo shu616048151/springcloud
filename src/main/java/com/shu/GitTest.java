31cd33536952138c81dba67976e8b74fb7380239
shu616048151 <616048151@qq.com>
2019-04-11 11:11:03 +0800

    修改新文件122212

--- src/main/java/com/shu/GitTest.java
+++ src/main/java/com/shu/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest{
     public static void main(String[] args){
         System.out.println("Git 修改文件测试提交");
         System.out.println("这是主分支和new test分支合并冲突修改");
+        System.out.println("sdfdsj");
 
     }
 }
