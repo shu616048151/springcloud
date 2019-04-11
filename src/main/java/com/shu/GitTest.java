0e24f4591b1e08eebc4fe466911af7794488866d
shu616048151 <616048151@qq.com>
2019-04-11 11:12:17 +0800

    工作目录更新

--- src/main/java/com/shu/GitTest.java
+++ src/main/java/com/shu/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest{
         System.out.println("Git 修改文件测试提交");
         System.out.println("这是主分支和new test分支合并冲突修改");
         System.out.println("sdfdsj");
+        System.out.println("工作目录更新");
 
     }
 }
