c4bfeed1d5955d03efbbb5b4c0d9e9fd11189a6d
shu616048151 <616048151@qq.com>
2019-04-11 10:31:17 +0800

    这是newbranch提交代码

--- src/main/java/com/shu/GitTest.java
+++ src/main/java/com/shu/GitTest.java
@@ -3,6 +3,6 @@ package com.shu;
 public class GitTest{
     public static void main(String[] args){
         System.out.println("Git 修改文件测试提交");
-     
+        System.out.println("这是newbranch的代码");
     }
 }
