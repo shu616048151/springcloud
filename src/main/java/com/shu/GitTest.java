7d186bf75d13f732db59463e24dd6190708241d6
shu616048151 <616048151@qq.com>
2019-04-11 10:47:55 +0800

    开始第一次提交文件

--- src/main/java/com/shu/GitTest.java
+++ src/main/java/com/shu/GitTest.java
@@ -2,7 +2,8 @@ package com.shu;
 
 public class GitTest{
     public static void main(String[] args){
-        System.out.println("Git 修改文件测试提交");
-        System.out.println("这是分支的代码");
+
+        System.out.println("冲突已经解决了");
+        System.out.println("new test");
     }
 }
