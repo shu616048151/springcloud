232e21d1b01ad5bc94934ecf36f1af6a94fb39e7
shu616048151 <616048151@qq.com>
2019-04-11 14:54:45 +0800

    maven项目重构

--- eurekaserver/src/main/java/com/shu/App.java
+++ eurekaserver/src/main/java/com/shu/App.java
@@ -1,10 +1,8 @@
 package com.shu;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
 @EnableEurekaServer
